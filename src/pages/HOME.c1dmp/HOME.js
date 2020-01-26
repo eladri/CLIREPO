8b93b770586dab2ace0c73451ea52239d237b9e3
elad rittal <eladrittal@gmail.com>
2020-01-26 12:13:54 +0200

    first commit

---
+++ src/pages/HOME.c1dmp/HOME.js
@@ -0,0 +1,13 @@
+// For full API documentation, including code examples, visit https://wix.to/94BuAAs
+
+$w.onReady(function () {
+	//TODO: write your page related code here...
+
+	console.log('from first user');
+	console.log('second log');
+	console.log('from local editor');
+
+	console.log('from new folder');
+	
+
+});
---
+++ src/pages/HOME.c1dmp/pageElements.d.ts
@@ -0,0 +1,13 @@
+type PageElementsMap = {
+  "#page1": $w.Page;
+  "#button1": $w.Button;
+  "#text3": $w.Text;
+  "#repeater1": $w.Repeater;
+  "#button2": $w.Button;
+  "#container1": $w.Container;
+  "#text1": $w.Text;
+  "#text2": $w.Text;
+  "#iconButton1": $w.Button;
+  "#header1": $w.Header;
+  "#footer1": $w.Footer;
+}
